8374002d3f489793dd1b016deac4648432ef6d5d
MadMincraftr <thean.mcgarity@gmail.com>
2024-02-23 15:54:51 -0600

    finishing up before release

--- src/UI/PScaleUIPopup.cpp
+++ src/UI/PScaleUIPopup.cpp
@@ -1,24 +1,27 @@
 #include <Geode\modify\CCLayer.hpp>
 #include <Geode\ui\Popup.hpp>
-#include <Geode/ui/TextInput.hpp>
+#include <Geode\binding\CCTextInputNode.hpp>
 #include "UI\PScaleInputEvents"
+#include <Geode/c++stl/string.hpp>
 using namespace geode::prelude;
 using namespace geode;
+using namespace gd;
 namespace ScalerUI
 {
 	class UIPopup : public Popup<std::string const&>
 	{
-        public: TextInput* inputBox;
+        public: CCTextInputNode* inputBox;
+		public: PScaleInputEventManager* inputDelegate;
 		protected: bool setup(std::string const& value) override
 		{
 			setTitle("Player Sizer", "bigFont.fnt", 0.925, 0);
 
 			auto winSize = CCDirector::get()->getWinSize();
-
-            inputBox = TextInput::create(150,"Player Size","bigFont.fnt");
+			inputDelegate = new PScaleInputEventManager();
+            inputBox = CCTextInputNode::create(100, 95,"Player Size","bigFont.fnt");
             inputBox->setPosition(winSize.width / 2, winSize.height / 2);
-			inputBox->setFilter("1234567890.");
-			inputBox->setDelegate(dynamic_cast<TextInputDelegate*>(new PScaleInputEventManager()));
+			inputBox->m_allowedChars = "1234567890.";
+			inputBox->m_delegate = inputDelegate;
 			addChild(inputBox);
 			return true;
 		};
